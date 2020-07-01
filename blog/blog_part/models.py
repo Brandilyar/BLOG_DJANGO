f9d3f14f57654382b07dff3785b8d2e72c1bbfe3
Paul <isakura313@gmail.com>
2020-07-01 20:51:09 +0300

    Initial commit

---
+++ blog/blog_part/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """ Так как это у нас модель, то мы  здесь определяем
+        через класс, какие у нас будут колонки
+    """
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', "Published"))
+    title = models.CharField(max_length= 250) #заголовок нашего поста
+    slug = models.SlugField(max_length=250, unique_for_date='publish') # здесь у нас будет путь нашего Поста
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'blog_post')
+    body = models.TextField() #основное содержимое нашего поста
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now= True)
+    status = models.CharField(max_length= 10, choices= STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
